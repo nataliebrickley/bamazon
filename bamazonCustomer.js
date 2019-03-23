500ca6c9bc429eb090fd77b57df87c9016cdd434
Natalie Brickley <natt_rose@yahoo.com>
2019-03-23 14:25:12 -0700

    connected to database and displayed the table to the terminal

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,29 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql');
-const table = require('console.table')
+const cTable = require('console.table')
+
+//connect to database:
+const connection = mysql.createConnection({
+    host: "localhost",
+    // Your port; if not 3306
+    port: 3306,
+    // Your username
+    user: "root",
+    // Your password
+    password: "password",
+    database: "bamazon"
+});
+
+connection.connect(err => {
+    if (err) throw err;
+    afterConnection()
+});
+
+function afterConnection() {
+    //display the table
+    connection.query("SELECT * FROM products", (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        connection.end();
+    })
+}
